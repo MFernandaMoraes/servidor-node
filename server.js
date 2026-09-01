37d8cc49014a65b6422b3168a1e9ee14b6972e96
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2026-09-01 09:33:59 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Importar o módulo nativo 'http' do Node.js
+const http = require('http');
+
+// Define o endereço (localhost - minha máquina local) e a porta onde o servidor vai escutar
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Cria o servidor web
+const server = http.createServer( (req, res) => {
+
+    // Define o status http como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
+
+}); 
