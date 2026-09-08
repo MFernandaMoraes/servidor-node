4dbbf9b96e189a731b71936544ea43055627fea4
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2026-09-08 08:36:31 -0300

    if

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ const port = 3000;
 // Cria o servidor web
 const server = http.createServer((req, res) => {
 
-  // Define o status HTTP como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
- 
-  // Envia a resposta para o navegador/cliente
-  res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n</h1>');
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        return res.end('<h1>Página Inicial</h1>'); // O return impede a execução  das linhas seguintes
+    }
+
+    if (req.url === '/alunos') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        return res.end('<h1>Lista de alunos </h1>');
+    }
+
+    // Define o status HTTP como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    
+    // Envia a resposta para o navegador/cliente
+    res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n</h1>');
 
 });
 
